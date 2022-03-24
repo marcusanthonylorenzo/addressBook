4175b44e58ad6d8d1efd81a5bb50768750e2f1ec
Marcus <marcanthonyzpadilla@gmail.com>
2022-03-24 07:52:11 -0700

    add output elements to a list

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,32 @@
 
 //UI logic
 $(document).ready(function () {
+  $("#form").submit(function (event) {
 
+    event.preventDefault();
 
+    const firstName = $("input#firstName").val();
+    const lastName = $("input#lastName").val();
+    const dob = parseInt($("input#DOB").val());
+    const streetAddr = $("input#streetAddr").val();
+    const city = $("input#city").val();
+    const state = $("input#state").val();
+    const email = $("input#email").val();
+    const phone = parseInt($("input#phone").val());
+    const output = $(".output");
 
+    output.prepend().html("<div>")
+      .append(
+        "<li>" + firstName +"</li>" +
+        "<li>" + lastName +"</li>" +
+        "<li>" + dob +"</li>"+
+        "<li>" + streetAddr +"</li>"+
+        "<li>" + city +"</li>"+
+        "<li>" + state +"</li>"+
+        "<li>" + email +"</li>"+
+        "<li>" + phone +"</li>"
+        );
+      
+
+  });
 });
